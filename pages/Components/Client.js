7f048ae6c29837bdfa3b542505f80fefe7d190eb
Dylan King <xylvnking@gmail.com>
2022-09-03 18:04:27 -0400

    mostly worked on fomr submission, need to edit data structure to include song name for convenience sake

--- pages/Components/Client.js
+++ pages/Components/Client.js
@@ -4,6 +4,9 @@ import firebase, {auth, db, provider, storage} from '../../firebase/clientApp'
 import { collection, getDocs, doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
 import {useAuthState} from "react-firebase-hooks/auth"
 
+import TextareaAutosize from 'react-textarea-autosize';
+
+
 
 export default function Client(props) {
     function check() {
@@ -21,6 +24,16 @@ export default function Client(props) {
                 querySnapshot.forEach((doc) => {
                     if (doc.id !== 'settings') {
                         setClientSongs(current => {
+
+
+
+                            // you could probably get the song name (document name) here, but it might change
+                            // the data structure which you're then iterating over
+                            // another option is to just save it with each song entry, 
+                            // which might be better tbh even if it's duplicate data - it is simpler
+
+
+
                             return [
                                 ...current,
                                 doc.data()
@@ -38,9 +51,22 @@ export default function Client(props) {
     }
 
     const saveRevisionNotes = (event) => {
+        const clientCollection = 'clientID' + props.userAuth.uid
         event.preventDefault()
-        console.log('updating firebase with revision notes...')
+        // console.log('updating firebase with revision notes...')
         const newRevisionNoteToBeSaved = event.target[0].value
+        // console.log(event.target[0].value)
+        console.log(`the revision note is: ${event.target[0].value}`)
+        console.log(`the song/document name is: ${event.target[1].value}`)
+        console.log(`collection to be updated is: ${clientCollection}`)
+
+
+        /*
+            you cut a corner and will pay the price now
+            you need to be able to reference the document name (song title)
+            here so that you can update it with the new revision note
+
+        */
 
     }
 
@@ -56,10 +82,10 @@ export default function Client(props) {
             <ul className={styles.clientSongList}>
                 {
                     clientSongs &&
-                    clientSongs.map((song, index) => {
+                    clientSongs.map((song, index1) => {
                         return (
-                            <ul key={index}className={styles.fileListItem}>
-                                {Object.values(song, index).map((songDataValue) => 
+                            <ul key={index1}className={styles.fileListItem}>
+                                {Object.values(song).map((songDataValue) => 
                                     <ul key={songDataValue.date} className={styles.fileVersion}>
                                         <li>{Date.parse(Date(songDataValue.date))}</li>
                                         {/* <li>{songDataValue.revisionNote}</li> */}
@@ -67,14 +93,17 @@ export default function Client(props) {
                                         <br />
                                         
                                         <form onSubmit={saveRevisionNotes}>
-                                            {/* <label for='revisionNote'>revision note</label> */}
-                                            <textarea
-                                                // id='revisionNote'
+                                            <br />
+                                            <TextareaAutosize 
                                                 defaultValue={songDataValue.revisionNote}
                                                 className={styles.revisionTextArea}
                                                 onChange={(e) => handleTyping(e.target.value, song)} 
-                                            >
-                                            </textarea>
+                                            />
+
+                                            {/* <input readOnly={true} value={Object.keys(song).map((songName) => songName)} style={{display: 'none'}}></input> */}
+                                            
+                                            <input readOnly={true} value={'this should be the song/document name'} style={{display: 'none'}}></input>
+                                            <br />
                                             <button type="submit">save changes</button>
                                             {/* <input type="button">suuub</input> */}
                                         </form>
